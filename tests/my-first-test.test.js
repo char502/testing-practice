cf4aa4704caa49132918fc4a02703522434afc7e
charlotte@virtualtrips.io <charlotte@virtualtrips.io>
2021-06-27 21:44:53 +0100

    adding first test

--- tests/my-first-test.test.js
+++ tests/my-first-test.test.js
@@ -1,4 +1,17 @@
-test("Example test", ()=>{
-    const sum = 1 + 2;
-    expect(sum).toBe(3);
-})
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import Index from '../pages/index';
+
+// test("Example test", ()=>{
+//     const sum = 1 + 2;
+//     expect(sum).toBe(3);
+// })
+
+describe('Index page', () => {
+    it('should render', () => {
+        render(<Index />);
+        const main = screen.getByRole('main');
+        expect(main).toBeInTheDocument();
+    })
+});
